3ebd342ccfb862c6dc68f97be60aa5b3c15cd209
Ander 🐺 <honoratoann@gmail.com>
2026-09-05 19:15:27 -0300

    feat(v1.3): ativa nova interface Android isolada

--- PCFlow-v1.3.0/android/app/src/main/java/com/ander/pcflow/MainActivity.kt
+++ PCFlow-v1.3.0/android/app/src/main/java/com/ander/pcflow/MainActivity.kt
@@ -8,6 +8,6 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         SessaoPcFlow.inicializar(this)
-        setContent { PcFlowApp() }
+        setContent { PcFlowV13App() }
     }
 }
