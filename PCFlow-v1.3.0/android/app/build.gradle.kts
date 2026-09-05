44e245aaae5e729b57fd072949e7b6bfd5a13e77
Ander 🐺 <honoratoann@gmail.com>
2026-09-05 19:15:34 -0300

    chore(v1.3): atualiza versão Android isolada

--- PCFlow-v1.3.0/android/app/build.gradle.kts
+++ PCFlow-v1.3.0/android/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.ander.pcflow"
         minSdk = 26
         targetSdk = 35
-        versionCode = 2
-        versionName = "0.2.0"
+        versionCode = 13
+        versionName = "1.3.0"
     }
 
     buildFeatures { compose = true }
